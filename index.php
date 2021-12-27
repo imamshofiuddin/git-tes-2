86200f33fb60aab88ce87721c063635946224395
Imam Shofiuddin <imamshofiuddin00@gmail.com>
2021-12-27 13:14:22 +0700

    Menambahkan paragraf

--- index.php
+++ index.php
@@ -8,5 +8,6 @@
 </head>
 <body>
     <h1>Hello World</h1>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laborum incidunt cupiditate a nesciunt. Vero tenetur magni fugiat quam odit earum, quo qui amet pariatur dolores voluptatem quaerat nemo rerum enim.</p>
 </body>
 </html>
